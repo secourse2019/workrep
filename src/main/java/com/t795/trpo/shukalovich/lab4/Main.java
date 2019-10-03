c89570d463342ccd0f0bb393fb0bd4a3241f0edb
Eugene Isakov <zhenja.isakov@gmail.com>
2019-10-03 10:41:27 +0300

    update

--- src/main/java/com/t795/trpo/shukalovich/lab4/Main.java
+++ src/main/java/com/t795/trpo/shukalovich/lab4/Main.java
@@ -3,6 +3,7 @@ package com.t795.trpo.shukalovich.lab4;
 public class Main {
     
     System.out.println("Hello")
-	System.out.println("Timofey lox");
+	System.out.println("Timofey lox")
+    int a;
 
 }
