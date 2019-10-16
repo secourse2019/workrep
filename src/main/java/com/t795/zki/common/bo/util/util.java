3269fd31c6d4dafabc380e1bbe5c81a3a7e591a9
timofey <timofey.zarenok@gmail.com>
2019-10-16 08:55:52 +0300

    add new func in util

--- src/main/java/com/t795/zki/common/bo/util/util.java
+++ src/main/java/com/t795/zki/common/bo/util/util.java
@@ -8,13 +8,25 @@ static class Util {
         second = temp;
     }
 
-    public static void Transpanition(BlockMessage cont) {
+    public static BlockMessage Transpanition(BlockMessage cont) {
         BlockMessage temp = new BlockMessage();
         temp.setRowsCount = cont.getRows;
         temp.setColumnsCount = cont.getCols;
         for(int i = 0; i != temp.getRows; ++i) {
             temp[i] = cont.getColumnByIndex(i);
         }
+        return temp;
+    }
+
+    public static int CircleValue(int current, int value) {
+        int sum = current+value;
+        if(sum < 26) {
+            return sum;
+        }
+        if(sum > 26) {
+            return sum - 26;
+        }
+        return sum;
     }
 }
 
