981493ce98a460108bbc04629a642ab8d3a8799a
vano hacker <v.gritskevich25@gmail.com>
2019-10-17 10:23:58 +0300

    added some logic to StreamMessage class

--- src/main/java/com/t795/zki/common/bo/message/StreamMessage.java
+++ src/main/java/com/t795/zki/common/bo/message/StreamMessage.java
@@ -1,4 +1,17 @@
 package com.t795.zki.common.bo.message;
 
 public class StreamMessage extends AbstractMessage {
+    private String message;
+
+    public  StreamMessage(String message) {
+        setMessage(message);
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String msg) {
+        this.message = msg;
+    }
 }
