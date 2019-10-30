c896f84a8ed522081f06d3fe3d9512e9c171dc8d
dnagafonov <denisagafonovwork@gmail.com>
2019-10-30 23:23:43 +0300

    hernya

---
+++ src/main/java/com/t795/zki/common/bo/table/table.java
@@ -0,0 +1,55 @@
+package com.t795.zki.ciphers;
+
+import com.t795.zki.common.bo.message.StreamMessage;
+
+public class TableMethod {
+
+    private String message;
+    private int value;
+    private char[] instance;
+
+    TableMethod(String message, int value) {
+        this.message = message;
+        this.value = value;
+        instance = message.toCharArray();
+    }
+
+
+    private void ChangeInstanse(int value) {
+        for(int i = 0; i != instance.length; ++i) {
+            instance[i] = (char)((int)rolling(value, i));
+        }
+    }
+
+
+    private int rolling(int value, int index) {
+        int number = ((int)instance[index] + value);
+
+        int lastCharacter = (int)'\uFFEF';
+
+        if (number > lastCharacter)
+        {
+            number -= lastCharacter;
+        }
+
+        if (number < 0) {
+            number += lastCharacter;
+        }
+        return number;
+    }
+
+
+    public StreamMessage getCipher() {
+        ChangeInstanse(this.value);
+        StreamMessage result = new StreamMessage( new String(instance) );
+        return result;
+    }
+
+    public StreamMessage getDeCipher() {
+        ChangeInstanse( -(this.value));
+        StreamMessage result = new StreamMessage( new String(instance) );
+        return result;
+    }
+
+}
+
