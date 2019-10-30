8666c0ec32e967b89f4199a52f95842942efadfe
KATESTYLES333 <43679820+KATESTYLES333@users.noreply.github.com>
2019-10-30 20:51:09 +0300

    Trying tests

--- src/main/java/com/t795/trpo/vyhouskaya/lab4/MagicSquareEncryptor.java
+++ src/main/java/com/t795/trpo/vyhouskaya/lab4/MagicSquareEncryptor.java
@@ -1,7 +1,7 @@
-package encryptor;
+package com.t795.trpo.vyhouskaya.lab4;
 
-public class MagicSquareEncryptor implements EncrypKeyCryptorMethod {
-    @Override
+public class MagicSquareEncryptor /* implements EncrypKeyCryptorMethod*/ {
+
     public void setMessage(String a) {
 
     }
