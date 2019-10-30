ec98eab976368d6fd6953193ff112ed9185e7ac2
Timofey <timaluk@icloud.com>
2019-10-30 20:35:17 +0300

    made caesar

--- src/main/java/com/t795/trpo/shukalovich/lab4/Main.java
+++ src/main/java/com/t795/trpo/shukalovich/lab4/Main.java
@@ -1,9 +1,9 @@
 package com.t795.trpo.shukalovich.lab4;
 
 public class Main {
-    int i=0;
+
     for(int i=0;i<10;++i)
 {
-i += 1;
+i += 1;int i=0;
 }
 }
---
+++ src/main/java/com/t795/zki/ciphers/SystemCaesar.java
@@ -0,0 +1,56 @@
+package com.t795.zki.ciphers;
+
+import com.t795.zki.common.bo.message.StreamMessage;
+
+public class SystemCaesar {
+
+        private String message;
+        private int value;
+        private char[] instance;
+
+        SystemCaesar(String message, int value) {
+            this.message = message;
+            this.value = value;
+            instance = message.toCharArray();
+        }
+
+
+        private void ChangeInstanse(int value) {
+            for(int i = 0; i != instance.length; ++i) {
+
+                instance[i] = (char)((int)rolling(value, i));
+
+            }
+        }
+
+
+        private int rolling(int value, int index) {
+            int number = ((int)instance[index] + value);
+
+            if (number > 65519)
+            {
+                number -= 65519;
+            }
+
+            if (number < 0) {
+                number += 65519;
+            }
+            return number;
+        }
+
+
+        public StreamMessage getCipher() {
+            ChangeInstanse(this.value);
+            StreamMessage result = new StreamMessage( new String(instance) );
+            return result;
+        }
+
+        public StreamMessage getDeCipher() {
+        ChangeInstanse( -(this.value));
+        StreamMessage result = new StreamMessage( new String(instance) );
+        return result;
+    }
+
+    }
+
+
