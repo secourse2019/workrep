80076787b3d58142a00623c43efd19f45ce82806
vorob <vorobyov_egor@bk.ru>
2019-10-30 23:46:29 +0300

    sum function testing

--- src/main/test/com.t795/trpo/vorobyov.lab4/CalculatorTest.java
+++ src/main/test/com.t795/trpo/vorobyov.lab4/CalculatorTest.java
@@ -12,31 +12,31 @@ public class CalculatorTest {
 
     @Test
     public void testDoubleSum(){
-        double result = Calculator.addNumbers(12d, 5d).doubleValue();
+        double result = Calculator.Sum(12d, 5d).doubleValue();
         Assert.assertEquals(12d, result);
     }
 
     @Test
     public void testFloatSum(){
-        float result = Calculator.addNumbers(12f, 5f).floatValue();
+        float result = Calculator.Sum(12f, 5f).floatValue();
         Assert.assertEquals(12f, result);
     }
 
     @Test
     public void testDoubleSum(){
-        double result = Calculator.addNumbers(12, 5d).doubleValue();
+        double result = Calculator.Sum(12, 5d).doubleValue();
         Assert.assertEquals(12d, result);
     }
 
     @Test
     public void testIntFloatSum(){
-        float r = Calculator.addNumbers(12, 5f).floatValue();
+        float r = Calculator.Sum(12, 5f).floatValue();
         Assert.assertEquals(12f, result);
     }
 
     @Test
     public void testFloatDoubleSum(){
-        double r = Calculator.addNumbers(10f, 5d).doubleValue();
+        double r = Calculator.Sum(10f, 5d).doubleValue();
         Assert.assertEquals(15d, result);
     }
 }
